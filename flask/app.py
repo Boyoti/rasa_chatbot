8e67799d8327e748dd3405ecd953f289a2c457f3
Brian Hopkins <brianhh1230@gmail.com>
2018-03-24 14:33:41 -0400

    updating for locale

--- flask/app.py
+++ flask/app.py
@@ -16,8 +16,8 @@ def chat():
     try:
         user_message = request.form["text"]
         print("This is the user message: {}".format(user_message))
-        print("The rasa host is: {}".format(rasa_host))
-        response = requests.get("http://" + rasa_host + ":5000/parse?q=" + user_message + "&project=chatbot")
+        print("The rasa host is: {}".format(rasa_host)
+        response = requests.get("http://" + rasa_host + ":5000/parse",params={"q":user_message, "project": "chatbot"})
         print(response.json())
         response = response.json()
         intent = response["intent"]
