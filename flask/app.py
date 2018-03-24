63dac95dee73314e12e0ae02833182a59504028b
Brian Hopkins <brianhh1230@gmail.com>
2018-03-24 14:15:07 -0400

    updating ports

--- flask/app.py
+++ flask/app.py
@@ -16,7 +16,7 @@ def chat():
     try:
         user_message = request.form["text"]
         print("This is the user message: {}".format(user_message))
-        response = requests.get("http://" + rasa_host + ":5005/parse",params={"q":user_message, "project": "chatbot"})
+        response = requests.get("http://" + rasa_host + ":5000/parse",params={"q":user_message, "project": "chatbot"})
         print(response.json())
         response = response.json()
         intent = response["intent"]
