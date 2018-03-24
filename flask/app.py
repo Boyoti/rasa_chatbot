ff1ce50aab3b9832f84b27d0970f638326d41ff3
Brian Hopkins <brianhh1230@gmail.com>
2018-03-24 15:18:26 -0400

    update output

--- flask/app.py
+++ flask/app.py
@@ -12,19 +12,22 @@ rasa_host = os.getenv("docker_host_ip")
 def hello_world():
     return 'Flask Dockerized'
 
-@app.route('/chat',methods=["POST"])
+@app.route('/chat')
 def chat():
-    print(request.form)
-    user_message = request.form["text"]
-    print("This is the user message: {}".format(user_message))
-    print("The rasa host is: {}".format(rasa_host))
-    url = "http://{}:5000/parse".format(rasa_host)
-    response = requests.get(url,params={"q":user_message, "project": "chatbot"})
-    print(response.json())
-    response = response.json()
-    intent = response["intent"]
-    print("Intent {}".format(intent))
-    return jsonify({"status": "success", "response": intent})
+    if 'question' in request.args:
+        print(request.args['question'])
+        user_message = request.args['question']
+        print("This is the user message: {}".format(user_message))
+        print("The rasa host is: {}".format(rasa_host))
+        url = "http://{}:5000/parse".format(rasa_host)
+        response = requests.get(url,params={"q":user_message, "project": "chatbot"})
+        print(response.json())
+        response = response.json()
+        intent = response["intent"]
+        print("Intent {}".format(intent))
+        return jsonify({"status": "success", "response": intent})
+    else:
+        return jsonify({"status": "failed", "response": "could not find question"})
 
 
 if __name__ == '__main__':
