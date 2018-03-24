1d80ce57851dbfccbd97f70fb3a6f27f0acb5d29
Brian Hopkins <brianhh1230@gmail.com>
2018-03-24 14:38:51 -0400

    fixed typo

--- flask/app.py
+++ flask/app.py
@@ -16,7 +16,7 @@ def chat():
     try:
         user_message = request.form["text"]
         print("This is the user message: {}".format(user_message))
-        print("The rasa host is: {}".format(rasa_host)
+        print("The rasa host is: {}".format(rasa_host))
         response = requests.get("http://" + rasa_host + ":5000/parse",params={"q":user_message, "project": "chatbot"})
         print(response.json())
         response = response.json()
