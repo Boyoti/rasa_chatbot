e9b6b405b37aa244ee0cd229100e8598e1caea77
Brian Hopkins <brianhh1230@gmail.com>
2018-03-24 20:34:50 -0400

    removing h1

--- flask/responses.py
+++ flask/responses.py
@@ -1,7 +1,7 @@
 import random
 
 # List of our hello and goodbye responses to pick at random
-greeting_response = ["hey", "hello", "hi", "sup","<h1>hello</h1>"]
+greeting_response = ["hey", "hello", "hi", "sup"]
 goodbye_response = ["have a good one", "goodbye", "bye", "later"]
 
 def greeting():
